0da8e9ad51c31e1adcd371e8a5512ed4399d42da
Felicitas Ralle <felicitasralle@gmail.com>
2024-07-01 23:39:22 -0300

    se completo el ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,10 @@
+numero1 = prompt('Ingrese un numero: ');
+numero2 = prompt('Ingrese otro numero: ');
+let mayor;
+if(numero1>numero2){
+    mayor = numero1;
+}
+else{
+    mayor = numero2;
+}
+alert('El mayor de los numeros ingresados es: '+mayor);
