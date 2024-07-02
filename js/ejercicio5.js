79ab8e245cc2d73510e4d587d1315a6d3d30b5f1
Felicitas Ralle <felicitasralle@gmail.com>
2024-07-01 23:33:25 -0300

    se completo el ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,3 @@
+const numero1 = parseInt(prompt('Ingrese un numero: '));
+const numero2 = parseInt(prompt('Ingrese otro numero: '));
+alert('El resultado de la suma de los dos numeros ingresados es: '+(numero1+numero2));
